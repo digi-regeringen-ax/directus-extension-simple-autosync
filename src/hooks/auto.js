4b33a7702607c221df0c1d2cacf64d7dac9b3e75
Andreas Bohman <mail@andreasbohman.se>
2025-03-19 10:46:57 +0100

    fix hooks

--- src/hooks/auto.js
+++ src/hooks/auto.js
@@ -1,5 +1,7 @@
 import {defineHook} from "@directus/extensions-sdk";
 import * as helpers from "../lib/helpers";
+import * as snapshot from "../lib/snapshot";
+import * as rights from "../lib/rights";
 
 export default defineHook(async ({init, action}, {services, getSchema, logger}) => {
     const {SchemaService, ServerService } = services;
@@ -31,6 +33,8 @@ export default defineHook(async ({init, action}, {services, getSchema, logger}) 
         "webhooks",
     ];
     const affectingActions = ["create", "update", "delete"];
+
+    const shouldIncludeRights = helpers.getEnvConfig().AUTOSYNC_INCLUDE_RIGHTS;
     
     const shouldAutoPull = helpers.getEnvConfig().AUTOSYNC_PULL;
     logger.info(`${helpers.LP} AUTOSYNC_PULL is ${shouldAutoPull}`);
@@ -57,7 +61,10 @@ export default defineHook(async ({init, action}, {services, getSchema, logger}) 
 
     async function doPush() {
         try {
-            await helpers.pushSnapshot(services, schema, accountability, false, versionData.version);
+            await snapshot.pushSnapshot(services, schema, accountability, false, versionData.version);
+            if(shouldIncludeRights) {
+                await rights.pushRights(services, schema, accountability, false, versionData.version);
+            }
         } catch (e) {
             logger.error(e, `${helpers.LP} doPush:`);
         }
@@ -65,7 +72,7 @@ export default defineHook(async ({init, action}, {services, getSchema, logger}) 
 
     async function doPull() {
         try {
-            await helpers.pullSyncFiles(services, schema, accountability, versionData.version);
+            await snapshot.pullSyncFiles(services, schema, accountability, versionData.version);
         } catch (e) {
             logger.error(e, `${helpers.LP} doPull:`);
         }
